0a01dbcd95578052e6bcd6d1488826d69333f5e6
wills-dev <ejioguvictor6@gmail.com>
2024-03-23 12:14:35 +0100

    styles for the home page

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+import Shirt from "./Shirt";
+import CameraRig from "./CameraRig";
+import Backdrop from "./Backdrop";
 
-const Canvas = () => {
-  return <div>Canvas</div>;
+const CanvasModel = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      {/* <Shirt />
+      <CameraRig />
+      <Backdrop /> */}
+    </Canvas>
+  );
 };
 
-export default Canvas;
+export default CanvasModel;
